435eb4c4478d529437212f368841007368531e85
Eknah <eknah@protonmail.com>
2022-06-07 11:01:53 +0200

    Make nullable, add comments and RemoveVehicle

--- Ovning5-Garage/Garage.cs
+++ Ovning5-Garage/Garage.cs
@@ -4,31 +4,44 @@ namespace Ovning5_Garage
 	internal class Garage<T> : IEnumerable<T> where T : Vehicle
 	{
 
-		private T[] entries;
+		private T?[] entries;
 
 		public Garage(int capacity)
 		{
 			entries = new T[capacity];
 		}
 
-		public IEnumerator<T> GetEnumerator()
+		public IEnumerator<T> GetEnumerator() // Allows iterating over a garage object
 		{
-			foreach (T e in entries)
+			foreach (T? e in entries)
 				if (e != null)
 					yield return e;
 		}
 
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator(); // Old un-generic IEnumerable function
 
 		internal bool AddVehicle(T newVehicle)
 		{
-			bool isFull = entries.All(e => e != null);
+			bool isFull = entries.All(e => e != null); // Check if all elements are assigned to
 
-			if (isFull) return false;
+			if (isFull) return false; // Cannot add vehicle if array is full
 
-			entries[entries.ToList().IndexOf(entries.First(e => e is null))] = newVehicle;
+			// Guaranteed to find a null element as the function would already have returned if the above isFull-check would have returned true.
+			var firstNullElementIndex = entries.ToList().IndexOf(entries.First(e => e is null));
 
-			return true;
+			entries[firstNullElementIndex] = newVehicle; // Place new vehicle in first empty array element
+
+			return true; // Add vehicle success
+		}
+
+		internal void RemoveVehicle(T vehicle)
+		{
+			var vehicleToRemoveIndex = entries.ToList().IndexOf(vehicle);
+
+			if (vehicleToRemoveIndex != -1)
+			{ 
+				entries[vehicleToRemoveIndex] = null;
+			}
 		}
 	}
 }
