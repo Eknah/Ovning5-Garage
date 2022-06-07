3ac258c0a95e22f73cf73274b27d414c97b1fb11
Eknah <eknah@protonmail.com>
2022-06-07 10:13:09 +0200

    Add comment and new  menu options

--- Ovning5-Garage/Helpers/MenuHelper.cs
+++ Ovning5-Garage/Helpers/MenuHelper.cs
@@ -1,15 +1,16 @@
 ﻿
 namespace Ovning5_Garage.Helpers
 {
-	internal class MenuHelper
+	internal class MenuHelper // Key bindings for menu options
 	{
 		public const ConsoleKey CreateNewGarage = ConsoleKey.D1;
-		public const ConsoleKey Quit = ConsoleKey.D2;
-		public const ConsoleKey ListAllVehicles = ConsoleKey.D3;
+		public const ConsoleKey ListAllVehicles = ConsoleKey.D2;
+		public const ConsoleKey ListVehicleTypes = ConsoleKey.D3;
 		public const ConsoleKey AddVehicle = ConsoleKey.D4;
 		public const ConsoleKey DeleteVehicle = ConsoleKey.D5;
 		public const ConsoleKey GetVehicleByRegNr = ConsoleKey.D6;
 		public const ConsoleKey GetVehiclesByProps = ConsoleKey.D7;
+		public const ConsoleKey Quit = ConsoleKey.D8;
 
 		public const ConsoleKey VehicleAirplane = ConsoleKey.D1;
 		public const ConsoleKey VehicleBoat = ConsoleKey.D2;
